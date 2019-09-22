5652afdbf4cc56d3e83b4f60735af859e4709f0a
Dave Moten <davidmoten@gmail.com>
2019-09-22 21:31:29 +1000

    add tests

--- src/test/java/logss/btree/BPlusTreeTest.java
+++ src/test/java/logss/btree/BPlusTreeTest.java
@@ -20,4 +20,19 @@ public class BPlusTreeTest {
         assertEquals("boo", t.find(1));
     }
 
+    @Test
+    public void testAddManyAndFind() {
+        for (int m = 4; m <= 10; m++) {
+            BPlusTree<Integer, String> t = BPlusTree.builder().maxKeys(m).naturalOrder();
+            for (int n = 1; n < 1000; n++) {
+                for (int i = 0; i < n; i++) {
+                    t.insert(i, "a" + i);
+                }
+                for (int i = n - 1; i >= 0; i--) {
+                    assertEquals("a" + i, t.find(i));
+                }
+            }
+        }
+    }
+
 }
