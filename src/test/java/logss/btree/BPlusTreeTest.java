aec2e46d9b6affaac3c3eb072711da0fe580035d
David Moten <davidmoten@gmail.com>
2019-09-26 12:12:58 +1000

    add splits test

--- src/test/java/logss/btree/BPlusTreeTest.java
+++ src/test/java/logss/btree/BPlusTreeTest.java
@@ -80,10 +80,18 @@ public class BPlusTreeTest {
             t.insert(i, i);
         }
         NodeWrapper<Integer, Integer> root = new NodeWrapper<Integer, Integer>(t.root());
-        assertEquals(Arrays.asList(3),root.keys());
+        assertEquals(Arrays.asList(3), root.keys());
         List<NodeWrapper<Integer, Integer>> children = root.children();
         assertEquals(2, children.size());
         assertEquals(Arrays.asList(2), children.get(0).keys());
         assertEquals(Arrays.asList(4), children.get(1).keys());
+        List<NodeWrapper<Integer, Integer>> a = children.get(0).children();
+        List<NodeWrapper<Integer, Integer>> b = children.get(1).children();
+        assertEquals(2, a.size());
+        assertEquals(Arrays.asList(1), a.get(0).keys());
+        assertEquals(Arrays.asList(2), a.get(1).keys());
+        assertEquals(2, b.size());
+        assertEquals(Arrays.asList(3), b.get(0).keys());
+        assertEquals(Arrays.asList(4, 5), b.get(1).keys());
     }
 }
