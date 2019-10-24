a6f9df86fcd1143242596a73b4e4ee8b394b46ad
David Moten <davidmoten@gmail.com>
2019-10-24 12:33:54 +1100

    change parameter name

--- src/main/java/logss/btree/Util.java
+++ src/main/java/logss/btree/Util.java
@@ -7,7 +7,7 @@ final class Util {
             // We are inserting a duplicate value, simply overwrite the old one
             leaf.setValue(idx, value);
         } else {
-            // TODO put at end of duplicate keys
+            // TODO put at end of duplicate keys?
             // so that iteration reflects sort order then addition order
             // The key we are inserting is unique
             leaf.insert(idx, key, value);
@@ -17,20 +17,20 @@ final class Util {
     
     static <K,V> void insertNonfull(NonLeaf<K,V> node, K key, V value) {
         // Simple linear search
-        int idx = node.getLocation(key);
-        Split<K, V> result = node.child(idx).insert(key, value);
+        int index = node.getLocation(key);
+        Split<K, V> result = node.child(index).insert(key, value);
 
         if (result != null) {
-            if (idx == node.numKeys()) {
+            if (index == node.numKeys()) {
                 // Insertion at the rightmost key
-                node.setKey(idx, result.key);
-                node.setChild(idx, result.left);
-                node.setChild(idx + 1, result.right);
+                node.setKey(index, result.key);
+                node.setChild(index, result.left);
+                node.setChild(index + 1, result.right);
             } else {
                 // Insertion not at the rightmost key
                 // shift i>idx to the right
-                node.insert(idx, result.key, result.left);
-                node.setChild(idx + 1, result.right);
+                node.insert(index, result.key, result.left);
+                node.setChild(index + 1, result.right);
             }
             node.setNumKeys(node.numKeys() + 1);
         } // else the current node is not affected
