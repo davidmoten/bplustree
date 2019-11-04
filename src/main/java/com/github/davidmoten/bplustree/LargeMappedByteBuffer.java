bfca7590096a8243c792af20fe3ac9226d26a79d
David Moten <davidmoten@gmail.com>
2019-11-05 10:52:26 +1100

    fix compile error

--- src/main/java/com/github/davidmoten/bplustree/LargeMappedByteBuffer.java
+++ src/main/java/com/github/davidmoten/bplustree/LargeMappedByteBuffer.java
@@ -1,7 +1,9 @@
 package com.github.davidmoten.bplustree;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.io.UncheckedIOException;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
@@ -36,18 +38,30 @@ public final class LargeMappedByteBuffer {
     private long position;
 
     private MappedByteBuffer bb(long position) {
-        long num = position % segmentSizeBytes;
+        long num = segmentNumber(position);
         Segment segment = map.get(num);
         if (segment == null) {
             File file = new File(directory, "data-" + num);
-            try (RandomAccessFile raf = new RandomAccessFile(file, "rw")) {
-                raf.setLength(segmentSizeBytes);
+            try {
+                try (RandomAccessFile raf = new RandomAccessFile(file, "rw")) {
+                    raf.setLength(segmentSizeBytes);
+                }
+                FileChannel channel = (FileChannel) Files.newByteChannel(file.toPath(), StandardOpenOption.CREATE,
+                        StandardOpenOption.READ, StandardOpenOption.WRITE);
+                MappedByteBuffer bb = channel.map(MapMode.READ_WRITE, 0, 1024 * 1024);
+
+                map.put(num, new Segment(channel, bb));
+                return bb;
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
             }
-            FileChannel channel = (FileChannel) Files.newByteChannel(file.toPath(), StandardOpenOption.CREATE, StandardOpenOption.READ, StandardOpenOption.WRITE);
-            MappedByteBuffer bb = channel.map(MapMode.READ_WRITE, 0, 1024*1024);
-            map.put(num, new Segment(channel, bb));
-            return bb;
+        } else {
+            return segment.bb;
         }
+    }
+
+    private long segmentNumber(long position) {
+        return position % segmentSizeBytes;
     }
 
     public void position(long newPosition) {
@@ -58,8 +72,8 @@ public final class LargeMappedByteBuffer {
         return bb(position++).get();
     }
 
-    public ByteBuffer put(byte b) {
-        return bb(position++).put(b);
+    public void put(byte b) {
+        bb(position++).put(b);
     }
 
     public byte get(long index) {
@@ -92,36 +106,43 @@ public final class LargeMappedByteBuffer {
     }
 
     public void putInt(int value) {
+        // TODO handle crossing multiple bb
         long p = position;
         position += Integer.SIZE;
         bb(p).putInt(value);
     }
 
     public int getInt(long index) {
+        // TODO handle crossing multiple bb
         return bb(index).getInt();
     }
 
     public void putInt(long index, int value) {
+        // TODO handle crossing multiple bb
         bb(index).putInt(value);
     }
 
     public long getLong() {
+        // TODO handle crossing multiple bb
         long p = position;
         position += Long.SIZE;
         return bb(p).getLong();
     }
 
     public void putLong(long value) {
+        // TODO handle crossing multiple bb
         long p = position;
         position += Long.SIZE;
         bb(p).putLong(value);
     }
 
     public long getLong(long index) {
+        // TODO handle crossing multiple bb
         return bb(index).getLong();
     }
 
     public void putLong(long index, long value) {
+        // TODO handle crossing multiple bb
         bb(index).putLong(value);
     }
 
