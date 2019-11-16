196d983de81ea5d9dececa7b1cb5df38d2f02f84
Dave Moten <davidmoten@gmail.com>
2019-11-16 22:04:26 +1100

    add benchmarks

--- src/test/java/com/github/davidmoten/bplustree/Benchmarks.java
+++ src/test/java/com/github/davidmoten/bplustree/Benchmarks.java
@@ -1,10 +1,13 @@
 package com.github.davidmoten.bplustree;
 
-import java.io.File;
-
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
 
 public class Benchmarks {
 
@@ -23,20 +26,38 @@ public class Benchmarks {
         return b;
     }
 
+    @State(Scope.Thread)
+    public static class MyState {
+
+        BPlusTree<Integer, Integer> tree;
+
+        @Setup(Level.Trial)
+        public void doSetup() {
+            tree = BPlusTree.file().directory("target/bench") //
+                    .clearDirectory() //
+                    .deleteOnClose() //
+                    .maxLeafKeys(16) //
+                    .segmentSizeMB(50) //
+                    .keySerializer(Serializer.INTEGER) //
+                    .valueSerializer(Serializer.INTEGER) //
+                    .naturalOrder();
+        }
+
+        @TearDown(Level.Trial)
+        public void doTearDown() {
+            try {
+                tree.close();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
     @Benchmark
     @BenchmarkMode(Mode.SingleShotTime)
-    public void storeOneMillionInts() throws Exception {
-        try (BPlusTree<Integer, Integer> tree = BPlusTree.file().directory("target/bench") //
-                .clearDirectory() //
-                .deleteOnClose() //
-                .maxLeafKeys(16) //
-                .segmentSizeMB(50) //
-                .keySerializer(Serializer.INTEGER) //
-                .valueSerializer(Serializer.INTEGER) //
-                .naturalOrder()) {
-            for (int i = 0; i < 1000000; i++) {
-                tree.insert(i, i);
-            }
+    public void storeOneMillionInts(MyState state) throws Exception {
+        for (int i = 0; i < 1000000; i++) {
+            state.tree.insert(i, i);
         }
     }
 
