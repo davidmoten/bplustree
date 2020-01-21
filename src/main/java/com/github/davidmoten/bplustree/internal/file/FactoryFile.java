903ef1d24efa12d2568ea3f461fd74e35f9cae3c
David Moten <davidmoten@gmail.com>
2020-01-21 12:04:45 +1100

    use leaf cache

--- src/main/java/com/github/davidmoten/bplustree/internal/file/FactoryFile.java
+++ src/main/java/com/github/davidmoten/bplustree/internal/file/FactoryFile.java
@@ -45,7 +45,7 @@ public final class FactoryFile<K, V> implements Factory<K, V> {
         this.onClose = onClose;
         this.bb = new LargeMappedByteBuffer(directory, segmentSizeBytes, "index-");
         this.values = new LargeMappedByteBuffer(directory, segmentSizeBytes, "value-");
-        this.leavesPool = createLeafPool(this, 10);
+        this.leavesPool = createLeafPool(this, 3);
     }
 
     //////////////////////////////////////////////////
@@ -72,6 +72,7 @@ public final class FactoryFile<K, V> implements Factory<K, V> {
     }
 
     private Leaf<K, V> getLeaf(long position) {
+//        return new LeafFile<K, V>(this, position);
         LeafFile<K, V> leaf = leavesPool.get(leavesIndex);
         leavesIndex = (leavesIndex + 1) % leavesPool.size();
         leaf.position(position);
@@ -255,6 +256,7 @@ public final class FactoryFile<K, V> implements Factory<K, V> {
         bb.position(pos);
         int type = bb.get();
         if (type == Leaf.TYPE) {
+//            return new LeafFile<K,V>(this, pos);
             return getLeaf(pos);
         } else {
             return new NonLeafFile<K, V>(this, pos);
